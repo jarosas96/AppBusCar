f91ca5d22e8e8da5975ee530bf9923863efa6a40
rosasjose96 <jose_alonso_rosas@outlook.com>
2022-10-23 07:24:46 -0500

    feat: basic Travel Tickets List structure

---
+++ app/src/main/java/com/dpa/esan/appbuscar/TravelTicketsListActivity.kt
@@ -0,0 +1,11 @@
+package com.dpa.esan.appbuscar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TravelTicketsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_travel_tickets_list)
+    }
+}
